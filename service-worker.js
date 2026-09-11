1407c2723fc7b7f7793567b3f0f349095375b6b3
maximemathon38-afk <maximemathon38@gmail.com>
2026-09-11 09:42:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="rbf-v5-private-depot";
+const CACHE="rbf-v5-1-public-depot-chef-transfer";
 const STATIC=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./rbf-logo.png","./rbf-banner.png"];
 
 self.addEventListener("install",event=>{
