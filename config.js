9ff5c87d1fb6c67518428b8e8d9c097a36f4f741
maximemathon38-afk <maximemathon38@gmail.com>
2026-09-11 10:18:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 window.RBF_CONFIG = {
   SUPABASE_URL: "https://uwqhwwjfroalbqctnyyf.supabase.co",
-
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_bu5tAfD8iK1Y0b2ggwJFpQ_-IANiDmk",
-
   REFRESH_INTERVAL_MS: 10000
 };
